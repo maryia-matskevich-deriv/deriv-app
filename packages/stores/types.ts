930634ca1dba6b2cf2ed2bfc1b891f33fa73888c
maryia-matskevich-deriv <maryia.frantsava@binary.com>
2023-03-27 13:06:58 +0300

    fix: type of redirectOnClick()

--- packages/stores/types.ts
+++ packages/stores/types.ts
@@ -200,7 +200,7 @@ type TCommonStoreError = {
     redirect_to: string;
     should_clear_error_on_click: boolean;
     should_show_refresh: boolean;
-    redirectOnClick: (() => void) | string;
+    redirectOnClick: () => void;
     setError: (has_error: boolean, error: TCommonStoreError | null) => void;
     app_routing_history: unknown[];
 };
